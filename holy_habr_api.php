773f096fb437e5d4871b6f277bf206a78a194c73
Newbilius <newbilius@gmail.com>
2013-02-24 21:37:09 +0600

    fix

--- holy_habr_api.php
+++ holy_habr_api.php
@@ -1,5 +1,13 @@
 <?php
-
+/**
+ * Парсер данных с ХабраХабра.
+ *
+ * Лицензия - делайте с файлом что хотите, только не кидайтесь помидорами и автора из файлов не выкидывайте.
+ *
+ * @author     Моисеев Дмитрий aka Newbilius (Nubilius) <newbilius@gmail.com>
+ * @copyright  2013
+ * @link       https://github.com/Newbilius/habr_to_fb2
+ */
 require('phpQuery-onefile.php');
 
 //@todo единообразные кавычки
